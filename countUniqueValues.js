167ea163b2df699d7c3096393549afcdc544108a
Jakedalus <jacquedalus@gmail.com>
2020-01-29 18:46:36 -0500

    Add my countUniqueValues solution

---
+++ countUniqueValues.js
@@ -0,0 +1,26 @@
+function countUniqueValues(arr) {
+
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return 1;
+
+  let uniqueValues = 1;
+
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 1; j < arr.length; j++) {
+      console.log('------------');
+      console.log(`${i}, ${j}`);
+      console.log(`${arr[i]}, ${arr[j]}`);
+      if (arr[i] !== arr[j]) {
+        uniqueValues++;
+        i = j;
+      }
+    }
+  }
+
+  return uniqueValues;
+
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2]));
+console.log(countUniqueValues([1,1,1,2,3,4,4,4,7,7,12,12,13]));
+console.log(countUniqueValues([1,1,1,1,2,2,3,3,3,3,4,4,4,7,7,12,12,13,13,15,15,16,16,16,17,200,200,201])); //12
