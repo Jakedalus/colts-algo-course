ea5a9959eeba673b2e0ed466986891616f330ec3
Jakedalus <jacquedalus@gmail.com>
2020-02-11 17:01:29 -0500

    Add maxSubarraySum naive solution

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -17,3 +17,31 @@ function naiveMaxSubarraySum(arr, num) {
 
 console.log(naiveMaxSubarraySum([1,2,5,2,8,1,5], 2)); // 10
 console.log(naiveMaxSubarraySum([1,2,5,2,8,1,5], 4)); // 17
+
+
+// Sliding Window Version
+function maxSubarraySum(arr, num) {
+
+  if (num > arr.length) return null;
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[num] + arr[i+1];
+    maxSum = Math.max(tempSum, maxSum);
+  }
+
+  return maxSum;
+
+}
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 2)); // 10
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 4)); // 17
+
+
+
