8400c3866924a9070e99f3eb43fae1b207378b2c
Jakedalus <jacquedalus@gmail.com>
2020-02-17 17:04:17 -0500

    Set up areThereDuplicates challenge

---
+++ areThereDuplicates.js
@@ -0,0 +1,22 @@
+// Frequency Counter version
+function areThereDuplicatesFC() {
+
+  
+
+}
+
+console.log(areThereDuplicatesFC(1, 2, 3)); // false
+console.log(areThereDuplicatesFC(1, 2, 2)); // true
+console.log(areThereDuplicatesFC('a', 'b', 'c', 'a')); // true
+
+
+// Multiple Pointes version
+function areThereDuplicatesMP() {
+
+  
+
+}
+
+console.log(areThereDuplicatesMP(1, 2, 3)); // false
+console.log(areThereDuplicatesMP(1, 2, 2)); // true
+console.log(areThereDuplicatesMP('a', 'b', 'c', 'a')); // true
