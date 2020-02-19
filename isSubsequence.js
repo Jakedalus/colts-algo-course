5e45586a559f172417f2e85064e19409bb39d59a
Jakedalus <jacquedalus@gmail.com>
2020-02-19 15:08:47 -0500

    Add Colt's solutions to isSubsequence and averagePair

--- isSubsequence.js
+++ isSubsequence.js
@@ -6,10 +6,13 @@ function isSubsequence(str1, str2) {
   for (let j = 0; j < str2.length; j++) {
     if (str1[i] === str2[j]) {
       i++;
+      if (i === str1.length) return true;
     }
   }
 
-  return i === str1.length;
+  return false;
+  
+  // return i === str1.length;
 
 }
 
@@ -17,3 +20,16 @@ console.log(isSubsequence('hello', 'hello world')); // true
 console.log(isSubsequence('sing', 'string')); // true
 console.log(isSubsequence('abc', 'abracadabra')); // true
 console.log(isSubsequence('abc', 'acb')); // false
+
+
+function isSubsequenceColt(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
