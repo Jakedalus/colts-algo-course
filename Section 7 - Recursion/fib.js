5800fb8f0ed64ad8771ee7a33f1817c35312545f
Jakedalus <jacquedalus@gmail.com>
2020-02-24 17:19:56 -0500

    Add another fib test

--- Section 7 - Recursion/fib.js
+++ Section 7 - Recursion/fib.js
@@ -7,7 +7,15 @@ function fib(n) {
 }
 
 console.log(fib(4)); // 3
+console.log(fib(5)); // 5
 console.log(fib(10)); // 55
 console.log(fib(28)); // 317811
 console.log(fib(35)); // 9227465
 
+
+
+// fib(5)
+//   fib(4)                           +             fib(3)
+//     fib(3)                 +     fib(2)             fib(2) + fib(1)
+//       fib(2) + fib(1)              1                   1       1
+//          1       1
