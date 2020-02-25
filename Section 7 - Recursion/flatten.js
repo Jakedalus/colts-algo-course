54f975c6f404b2115bfcf040187d47fb15562aac
Jakedalus <jacquedalus@gmail.com>
2020-02-25 16:38:16 -0500

    Add Colt's iterative-recursive solution to flatten

--- Section 7 - Recursion/flatten.js
+++ Section 7 - Recursion/flatten.js
@@ -51,4 +51,18 @@ console.log(flattenRecursiveHelper([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,
 
 
 
+function coltIterativeRecursiveFlatten(oldArr){
+  var newArr = []
+    for(var i = 0; i < oldArr.length; i++){
+      if(Array.isArray(oldArr[i])){
+          newArr = newArr.concat(flatten(oldArr[i]))
+      } else {
+          newArr.push(oldArr[i])
+      }
+  } 
+  return newArr;
+}
+
+
+
 
