1e80f0fc0c7206f8444ed02ec32c33e71fa4c728
Jakedalus <jacquedalus@gmail.com>
2020-03-02 15:46:46 -0500

    Add bubbleSort

---
+++ Section 11 - Sorting Algorithms/bubbleSort.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr) {
+
+  let sorted = arr.slice();
+
+  function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+  }
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (sorted[j] > sorted[j + 1]) swap(sorted, j, j + 1);
+    }
+  }
+
+  return sorted;
+}
+
+console.log(bubbleSort([45,23,1,4,6,2231,31,-12]));
+console.log(bubbleSort([1,2,3,4,5]));
+console.log(bubbleSort([5,3,2,1,-1]));
+console.log(bubbleSort([3, 46, -2, 0, 1, 019, -23, 12, 634, 1, 6, 61]));
