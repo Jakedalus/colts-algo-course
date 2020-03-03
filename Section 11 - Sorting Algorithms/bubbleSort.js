e79e9289baef7a8e6705942568d4ba8b06e82b78
Jakedalus <jacquedalus@gmail.com>
2020-03-03 16:43:46 -0500

    Fix arr bounds error in bubbleSort

--- Section 11 - Sorting Algorithms/bubbleSort.js
+++ Section 11 - Sorting Algorithms/bubbleSort.js
@@ -7,15 +7,18 @@ function bubbleSort(arr) {
   }
 
   for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(sorted, sorted[j], sorted[j+1]);
       if (sorted[j] > sorted[j + 1]) swap(sorted, j, j + 1);
     }
+    console.log("-----");
   }
 
   return sorted;
 }
 
-console.log(bubbleSort([45,23,1,4,6,2231,31,-12]));
-console.log(bubbleSort([1,2,3,4,5]));
-console.log(bubbleSort([5,3,2,1,-1]));
-console.log(bubbleSort([3, 46, -2, 0, 1, 019, -23, 12, 634, 1, 6, 61]));
+// console.log(bubbleSort([45,23,1,4,6,2231,31,-12]));
+// console.log(bubbleSort([1,2,3,4,5]));
+// console.log(bubbleSort([5,3,2,1,-1]));
+console.log(bubbleSort([34,65,12,-24,54]));
+// console.log(bubbleSort([3, 46, -2, 0, 1, 019, -23, 12, 634, 1, 6, 61]));
