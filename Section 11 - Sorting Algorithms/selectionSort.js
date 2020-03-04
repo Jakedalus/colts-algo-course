45957a94b84f7417898833a7182c733ef8bd4977
Jakedalus <jacquedalus@gmail.com>
2020-03-04 13:54:53 -0500

    Some cleanup on selectionSort

--- Section 11 - Sorting Algorithms/selectionSort.js
+++ Section 11 - Sorting Algorithms/selectionSort.js
@@ -3,11 +3,9 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[min]) {
-        min = j;
-      }
+      if (arr[j] < arr[min]) min = j;
     }
-    [arr[min], arr[i]] = [arr[i], arr[min]];
+    if (min !== i) [arr[min], arr[i]] = [arr[i], arr[min]];
   }
 
   return arr;
