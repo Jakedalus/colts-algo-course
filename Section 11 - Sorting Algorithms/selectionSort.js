040f7017cb9d2abf423394ca91b22fc17b968383
Jakedalus <jacquedalus@gmail.com>
2020-03-04 13:42:35 -0500

    Add selectionSort

---
+++ Section 11 - Sorting Algorithms/selectionSort.js
@@ -0,0 +1,25 @@
+function selectionSort(arr) {
+
+  
+
+  for (let i = 0; i < arr.length; i++) {
+    let minVal = arr[i], minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < minVal) {
+        minVal = arr[j];
+        minIndex = j;
+      }
+    }
+    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+
+  return arr;
+
+}
+
+console.log(selectionSort([45,23,1,4,6,2231,31,-12]));
+console.log(selectionSort([1,2,3,4,5]));
+console.log(selectionSort([5,3,2,1,-1]));
+console.log(selectionSort([34,65,12,-24,54]));
+console.log(selectionSort([3, 46, -2, 0, 1, 019, -23, 12, 634, 1, 6, 61]));
+console.log(selectionSort([8,1,2,3,4,5,7]));
