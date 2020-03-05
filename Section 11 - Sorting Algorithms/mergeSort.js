1662701e4c208dbd330df560ec5c0e49deba1d09
Jakedalus <jacquedalus@gmail.com>
2020-03-05 15:04:40 -0500

    Add mergeSort

--- Section 11 - Sorting Algorithms/mergeSort.js
+++ Section 11 - Sorting Algorithms/mergeSort.js
@@ -27,4 +27,23 @@ function merge(arr1, arr2) {
 
 }
 
-console.log(merge([1,10,50], [2,14,99,100]));
+// console.log(merge([1,10,50], [2,14,99,100]));
+
+function mergeSort(arr) {
+
+  if (arr.length <= 1) return arr;
+
+  return merge(mergeSort(arr.slice(0, arr.length/2)), mergeSort(arr.slice(arr.length/2)));
+
+}
+
+console.log(mergeSort([10,2,-3,22,24,5,56]));
+console.log(mergeSort([100,-20,-3,22,124,-5,56,123]));
+console.log(mergeSort([1,2,3,4,5,6]));
+console.log(mergeSort([10]));
+console.log(mergeSort([]));
+console.log(mergeSort([10,9,8,7,6,5]));
+
+
+
+
