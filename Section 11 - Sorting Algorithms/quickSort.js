bfee7da74466c3a6df06056735a82d58ca8b52ad
Jakedalus <jacquedalus@gmail.com>
2020-03-06 14:27:43 -0500

    Add full quickSort implementation

--- Section 11 - Sorting Algorithms/quickSort.js
+++ Section 11 - Sorting Algorithms/quickSort.js
@@ -1,6 +1,6 @@
 function partition(arr, start, end) {
 
-  let pivot = arr[0], pivotIndex = 0;
+  let pivot = arr[start], pivotIndex = start;
 
   for (let i = start + 1; i < end; i++) {
     if (arr[i] < pivot) {
@@ -9,11 +9,45 @@ function partition(arr, start, end) {
     }
   }
 
-  [arr[0], arr[pivotIndex]] = [arr[pivotIndex], arr[0]];
+  [arr[start], arr[pivotIndex]] = [arr[pivotIndex], arr[start]];
 
+
+  // console.log("partitioned:", arr);
+
+  return pivotIndex;
+
+}
+
+// console.log(partition([5,2,64,-12,52], 0));
+// console.log(partition([4,6,1,2,5,8,3,7], 0));
+// console.log(partition([8,6,1,2,5,4,3,7], 0));
+// console.log(partition([1,6,8,2,5,4,3,7], 0));
+
+function quickSortContainer(arr) {
+
+  function quickSort(arr, start, end) {
+    // console.log('----');
+    // console.log('arr', arr);
+    // console.log('start:', start, 'end:', end);
+    if (end <= start) return;
+
+    let pivot = partition(arr, start, end);
+
+    // console.log("pivot:", pivot);
+
+    quickSort(arr, start, pivot);
+    quickSort(arr, pivot + 1, end);
+
+  }
+
+  quickSort(arr, 0, arr.length);
 
   return arr;
 
 }
 
-console.log(partition([5,2,64,-12,52], 0, 5));
+console.log(quickSortContainer([5,2,64,-12,52]));
+console.log(quickSortContainer([4,6,1,2,5,8,3,7]));
+console.log(quickSortContainer([8,6,1,2,5,4,3,7]));
+console.log(quickSortContainer([1,6,8,2,5,4,3,7]));
+
