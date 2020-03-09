9fb0d8080fee376920d7f8059f7c24a5cd1be313
Jakedalus <jacquedalus@gmail.com>
2020-03-09 14:04:31 -0400

    Add digitCount to radix sort

--- Section 11 - Sorting Algorithms/radixSort.js
+++ Section 11 - Sorting Algorithms/radixSort.js
@@ -9,3 +9,13 @@ console.log(getDigit(12345,3));
 console.log(getDigit(12345,4));
 console.log(getDigit(12345,5));
 
+function digitCount(num) {
+  return Math.max(Math.floor(Math.log10(Math.abs(num))), 0) + 1;
+}
+
+console.log(digitCount(3));
+console.log(digitCount(32));
+console.log(digitCount(334));
+console.log(digitCount());
+console.log(digitCount(0));
+
