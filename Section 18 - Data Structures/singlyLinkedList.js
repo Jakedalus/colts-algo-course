d29d32802d988f4a2b8083e589c6dba4dac930e6
Jakedalus <jacquedalus@gmail.com>
2020-03-16 16:07:34 -0400

    Add remove method to SinglyLinkedList

--- Section 18 - Data Structures/singlyLinkedList.js
+++ Section 18 - Data Structures/singlyLinkedList.js
@@ -106,12 +106,10 @@ class SinglyLinkedList {
 
   insert(val, index) {
     if (index < 0 || index > this.length) return false;
-    
-    let newNode = new Node(val);
-
     if (index === this.length) return !!this.push(val);
     if (index === 0) return !!this.unshift(val);
 
+    let newNode = new Node(val);
     let pre = this.get(index - 1);
     newNode.next = pre.next;
     pre.next = newNode;
@@ -120,32 +118,58 @@ class SinglyLinkedList {
     return true;
   }
 
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let pre = this.get(index - 1);
+    let removed = this.get(index);
+    pre.next = removed.next;
+
+    return removed;
+  }
+
 }
 
 
 
 let list = new SinglyLinkedList();
+
+
 console.log(list.push('hello!'));
 console.log(list.push('middle node!'));
 console.log(list.push('goodbye!'));
 console.log(list.head.next);
+console.log('-----');
+
 console.log('pop:', list.pop());
 console.log(list);
+console.log('-----');
+
 console.log('shift:', list.shift());
 console.log(list);
 console.log('shift:', list.shift());
 console.log(list);
+console.log('-----');
+
 console.log('unshift:', list.unshift('new head!'));
 console.log('unshift:', list.unshift('second new head!'));
+console.log('-----');
+
 console.log('get:', list.get(0));
 console.log('get:', list.get(1));
 console.log(list);
 console.log('get:', list.get(2));
 console.log('get:', list.get(3));
+console.log('-----');
+
 console.log('set:', list.set('this will not work', 3));
 console.log(list);
 console.log('set:', list.set('changeinggg', 1));
 console.log(list);
+console.log('-----');
+
 console.log('insert:', list.insert('first', 0));
 console.log(list);
 console.log('insert:', list.insert('last', 3));
@@ -157,7 +181,18 @@ console.log(list.get(1));
 console.log(list.get(2));
 console.log(list.get(3));
 console.log(list.get(4));
+console.log('-----');
 
+console.log(list.remove(0));
+console.log(list.get(0));
+console.log(list);
+console.log(list.remove(3));
+console.log(list.get(2));
+console.log(list);
+console.log(list.remove(1));
+console.log(list.get(1));
+console.log(list);
+console.log('-----');
 
 
 console.log('-----');
