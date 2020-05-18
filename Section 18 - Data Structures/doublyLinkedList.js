a9f5733d7a30a933d2dfeaacfcdd412117403638
Jakedalus <jacquedalus@gmail.com>
2020-05-18 15:21:24 -0400

    Add push method to DoublyLinkedList

--- Section 18 - Data Structures/doublyLinkedList.js
+++ Section 18 - Data Structures/doublyLinkedList.js
@@ -12,4 +12,38 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
 }
+
+console.log("========");
+console.log("Initialize List");
+console.log("--------");
+const list = new DoublyLinkedList();
+console.log(list);
+
+console.log();
+console.log("========");
+console.log("Push nodes onto list");
+console.log("--------");
+list.push(15);
+console.log(list);
+list.push(11);
+console.log(list);
+list.push(-9);
+console.log(list);
+console.log("--------");
