7da15b036c726ef6a3dad190766fad5ccab88d10
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-01 14:11:21 -0400

    Add my solution to insert for BinarySearchTree

---
+++ Section 22 - BSTs/binarySearchTree.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(7);
+bst.insert(6);
+bst.insert(15);
+bst.insert(19);
+bst.insert(17);
+bst.insert(16);
+console.log(bst);
