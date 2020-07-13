38f58a8b19d9a92370522a8ba75e3985561757d8
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-13 14:41:46 -0400

    Add my solution to BFS, using BST as the tree

--- Section 22 - BSTs/binarySearchTree.js
+++ Section 22 - BSTs/binarySearchTree.js
@@ -84,33 +84,35 @@ class BinarySearchTree {
   }
 }
 
-const bst = new BinarySearchTree();
-console.log(bst.insert(10));
-console.log(bst.insert(7));
-console.log(bst.insert(6));
-console.log(bst.insert(15));
-console.log(bst.insert(19));
-console.log(bst.insert(17));
-console.log(bst.insert(16));
-console.log(bst.insert(10));
-console.log(bst);
+module.exports = BinarySearchTree;
 
-console.log("------- find -------");
-console.log(bst.find(10));
-console.log("----");
-console.log(bst.find(17));
-console.log("----");
-console.log(bst.find(6));
-console.log("----");
-console.log(bst.find(160));
-console.log("----");
+// const bst = new BinarySearchTree();
+// console.log(bst.insert(10));
+// console.log(bst.insert(7));
+// console.log(bst.insert(6));
+// console.log(bst.insert(15));
+// console.log(bst.insert(19));
+// console.log(bst.insert(17));
+// console.log(bst.insert(16));
+// console.log(bst.insert(10));
+// console.log(bst);
 
-console.log("------- colt's find -------");
-console.log(bst.coltsFind(10));
-console.log("----");
-console.log(bst.coltsFind(17));
-console.log("----");
-console.log(bst.coltsFind(6));
-console.log("----");
-console.log(bst.coltsFind(160));
-console.log("----");
+// console.log("------- find -------");
+// console.log(bst.find(10));
+// console.log("----");
+// console.log(bst.find(17));
+// console.log("----");
+// console.log(bst.find(6));
+// console.log("----");
+// console.log(bst.find(160));
+// console.log("----");
+
+// console.log("------- colt's find -------");
+// console.log(bst.coltsFind(10));
+// console.log("----");
+// console.log(bst.coltsFind(17));
+// console.log("----");
+// console.log(bst.coltsFind(6));
+// console.log("----");
+// console.log(bst.coltsFind(160));
+// console.log("----");
