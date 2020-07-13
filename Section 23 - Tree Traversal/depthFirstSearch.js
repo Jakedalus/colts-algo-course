f638cd2c14e4c6a28abd088bfb64a7b73746d3d4
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-13 15:58:47 -0400

    Add my solutino to DFS pre-order

---
+++ Section 23 - Tree Traversal/depthFirstSearch.js
@@ -0,0 +1,25 @@
+var BST = require("../Section 22 - BSTs/binarySearchTree");
+
+const bst = new BST();
+bst.insert(10);
+bst.insert(6);
+bst.insert(15);
+bst.insert(3);
+bst.insert(8);
+bst.insert(20);
+
+function dfs(tree) {
+  const visited = [];
+
+  function traverse(node) {
+    visited.push(node.value);
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+  }
+
+  traverse(tree.root);
+
+  return visited;
+}
+
+console.log(dfs(bst));
