c7d25123bb0bd17e9cb172177323a343bb4115de
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-15 17:05:45 -0400

    Fix edge case in extractMax for heap

--- Section 24 - Heaps/maxBinaryHeap.js
+++ Section 24 - Heaps/maxBinaryHeap.js
@@ -28,10 +28,22 @@ class MaxBinaryHeap {
     const max = this.values.pop();
 
     let parentIndex = 0;
-    let largestChildIndex =
-      this.values[2 * parentIndex + 1] > this.values[2 * parentIndex + 2]
-        ? 2 * parentIndex + 1
-        : 2 * parentIndex + 2;
+    let leftChildIndex = 2 * parentIndex + 1;
+    let rightChildIndex = 2 * parentIndex + 2;
+    let largestChildIndex;
+    if (leftChildIndex > this.values.length - 1) {
+      if (rightChildIndex > this.values.length - 1) {
+        return max;
+      }
+      largestChildIndex = rightChildIndex;
+    } else if (rightChildIndex > this.values.length - 1) {
+      largestChildIndex = leftChildIndex;
+    } else {
+      largestChildIndex =
+        this.values[leftChildIndex] > this.values[rightChildIndex]
+          ? leftChildIndex
+          : rightChildIndex;
+    }
 
     // console.log(temp, parentIndex, largestChildIndex, this.values);
 
@@ -41,32 +53,59 @@ class MaxBinaryHeap {
       this.values[parentIndex] = this.values[largestChildIndex];
       this.values[largestChildIndex] = temp;
       parentIndex = largestChildIndex;
-      largestChildIndex =
-        this.values[2 * parentIndex + 1] > this.values[2 * parentIndex + 2]
-          ? 2 * parentIndex + 1
-          : 2 * parentIndex + 2;
+      leftChildIndex = 2 * parentIndex + 1;
+      rightChildIndex = 2 * parentIndex + 2;
+      if (leftChildIndex > this.values.length - 1) {
+        if (rightChildIndex > this.values.length - 1) {
+          break;
+        }
+        largestChildIndex = rightChildIndex;
+      } else if (rightChildIndex > this.values.length - 1) {
+        largestChildIndex = leftChildIndex;
+      } else {
+        largestChildIndex =
+          this.values[leftChildIndex] > this.values[rightChildIndex]
+            ? leftChildIndex
+            : rightChildIndex;
+      }
     }
 
     return max;
   }
 }
 
+// const heap = new MaxBinaryHeap();
+// console.log(heap.insert(47));
+// console.log(heap.insert(49));
+// console.log(heap.insert(12));
+// console.log(heap.insert(102));
+// console.log(heap.insert(1));
+// console.log(heap.insert(35));
+// console.log(heap.insert(50));
+// console.log(heap.insert(350));
+
 const heap = new MaxBinaryHeap();
-console.log(heap.insert(47));
-console.log(heap.insert(49));
-console.log(heap.insert(12));
-console.log(heap.insert(102));
-console.log(heap.insert(1));
-console.log(heap.insert(35));
-console.log(heap.insert(50));
-console.log(heap.insert(350));
+heap.insert(55);
+heap.insert(33);
+heap.insert(39);
+heap.insert(41);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
 
 console.log("-------");
 console.log(heap.extractMax());
 console.log(heap.extractMax());
 console.log(heap.extractMax());
+console.log("***");
 console.log(heap.extractMax());
+console.log(heap);
+console.log("***");
 console.log(heap.extractMax());
+console.log(heap);
+console.log("***");
 console.log(heap.extractMax());
+console.log(heap);
+console.log("***");
 console.log(heap.extractMax());
 console.log(heap);
