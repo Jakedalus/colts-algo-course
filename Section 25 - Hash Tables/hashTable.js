037ed35cb3729544403fba7acf8c4f178e1e6111
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-29 18:10:16 -0400

    Add my solution to Keys and Values methods for HashTable class

--- Section 25 - Hash Tables/hashTable.js
+++ Section 25 - Hash Tables/hashTable.js
@@ -81,6 +81,7 @@ class HashTable {
 
 const ht = new HashTable(17);
 ht.set('jake', ' is super cool!');
+ht.set('jake2', ' is super cool!');
 ht.set('bob', 9823094823);
 ht.set('francis', ' sucks!!!');
 ht.set('Julie', ' jumps very high');
