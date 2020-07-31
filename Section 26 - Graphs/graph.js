421d829b8470777956ec13c250d9660960e1bb45
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-31 15:43:58 -0400

    Add addVertex to Graph class

--- Section 26 - Graphs/graph.js
+++ Section 26 - Graphs/graph.js
@@ -2,4 +2,16 @@ class Graph {
 	constructor() {
 		this.adjacencyList = {};
 	}
+
+	addVertex(vertex) {
+		if (!this.addVertex[vertex]) {
+			this.adjacencyList[vertex] = [];
+		}
+	}
 }
+
+const g = new Graph();
+
+g.addVertex('Tokyo');
+g.addVertex('San Fran');
+console.log(g);
