b07f082cfb12ff76d0867efdd7aebf039bbd2135
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-31 16:13:47 -0400

    Add my solution to removeVertex for Graph class

--- Section 26 - Graphs/graph.js
+++ Section 26 - Graphs/graph.js
@@ -33,6 +33,16 @@ class Graph {
 			].filter(v => v !== vertex1);
 		}
 	}
+
+	removeVertex(vertex) {
+		if (this.adjacencyList[vertex]) {
+			this.adjacencyList[vertex].forEach(v => {
+				this.removeEdge(vertex, v);
+			});
+		}
+
+		delete this.adjacencyList[vertex];
+	}
 }
 
 const g = new Graph();
@@ -43,15 +53,22 @@ g.addEdge('Tokyo', 'San Fran');
 console.log(g);
 console.log('----- ERROR HANDLING -----');
 g.addVertex('Tokyo');
-console.log(g);
+console.log(g); // Should not overwrite Tokyo's list
 g.addEdge('Tokyo', 'Dallas');
-console.log(g);
+console.log(g); // Shouldn't work since Dallas hasn't been added as a vertex yet
 console.log('----- REMOVING -----');
 g.addVertex('Dallas');
 g.addVertex('Philly');
 g.addEdge('San Fran', 'Philly');
 g.addVertex('Shanghai');
 g.addEdge('Tokyo', 'Shanghai');
+g.addVertex('Hong Kong');
+g.addEdge('Tokyo', 'Hong Kong');
+g.addEdge('Hong Kong', 'Shanghai');
 console.log(g);
-g.removeEdge('San Fran', 'Tokyo');
+g.removeEdge('San Fran', 'Philly');
 console.log(g);
+g.removeVertex('Dallas');
+console.log(g);
+g.removeVertex('Tokyo');
+console.log(g); // Should remove all edges also
