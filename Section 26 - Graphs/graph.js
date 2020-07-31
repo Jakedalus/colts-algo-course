4f1931ab0003b85840e388e7f5a9838137cd4d4d
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-31 15:51:14 -0400

    Add addEdge method to Graph class

--- Section 26 - Graphs/graph.js
+++ Section 26 - Graphs/graph.js
@@ -1,11 +1,22 @@
 class Graph {
+	// undirected, unweighted graph
 	constructor() {
 		this.adjacencyList = {};
 	}
 
 	addVertex(vertex) {
-		if (!this.addVertex[vertex]) {
+		if (!this.adjacencyList[vertex]) {
 			this.adjacencyList[vertex] = [];
+		}
+	}
+
+	addEdge(vertex1, vertex2) {
+		if (
+			this.adjacencyList[vertex1] &&
+			this.adjacencyList[vertex2]
+		) {
+			this.adjacencyList[vertex1].push(vertex2);
+			this.adjacencyList[vertex2].push(vertex1);
 		}
 	}
 }
@@ -14,4 +25,10 @@ const g = new Graph();
 
 g.addVertex('Tokyo');
 g.addVertex('San Fran');
+g.addEdge('Tokyo', 'San Fran');
+console.log(g);
+console.log('----- ERROR HANDLING -----');
+g.addVertex('Tokyo');
+console.log(g);
+g.addEdge('Tokyo', 'Dallas');
 console.log(g);
