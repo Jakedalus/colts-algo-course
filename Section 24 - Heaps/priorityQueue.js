2dbc8e3551a975a572d5d6389a26fded9d42313b
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-14 14:42:50 -0400

    Add setup and initialization for Dijkstra, export PriorityQueue and include it

--- Section 24 - Heaps/priorityQueue.js
+++ Section 24 - Heaps/priorityQueue.js
@@ -1,132 +1,137 @@
 class Node {
-  constructor(val, priority) {
-    this.val = val;
-    this.priority = priority;
-  }
+	constructor(val, priority) {
+		this.val = val;
+		this.priority = priority;
+	}
 }
 
 class PriorityQueue {
-  constructor() {
-    this.values = [];
-  }
+	constructor() {
+		this.values = [];
+	}
 
-  enqueue(value, priority) {
-    const newNode = new Node(value, priority);
-    this.values.push(newNode);
+	enqueue(value, priority) {
+		const newNode = new Node(value, priority);
+		this.values.push(newNode);
 
-    let index = this.values.length - 1;
-    let parentIndex = Math.floor((index - 1) / 2);
+		let index = this.values.length - 1;
+		let parentIndex = Math.floor((index - 1) / 2);
 
-    while (
-      parentIndex >= 0 &&
-      this.values[parentIndex].priority > this.values[index].priority
-    ) {
-      let temp = this.values[parentIndex];
-      this.values[parentIndex] = this.values[index];
-      this.values[index] = temp;
-      index = parentIndex;
-      parentIndex = Math.floor((index - 1) / 2);
-    }
+		while (
+			parentIndex >= 0 &&
+			this.values[parentIndex].priority >
+				this.values[index].priority
+		) {
+			let temp = this.values[parentIndex];
+			this.values[parentIndex] = this.values[index];
+			this.values[index] = temp;
+			index = parentIndex;
+			parentIndex = Math.floor((index - 1) / 2);
+		}
 
-    return this.values;
-  }
+		return this.values;
+	}
 
-  dequeue() {
-    let temp = this.values[0];
-    this.values[0] = this.values[this.values.length - 1];
-    this.values[this.values.length - 1] = temp;
+	dequeue() {
+		let temp = this.values[0];
+		this.values[0] = this.values[this.values.length - 1];
+		this.values[this.values.length - 1] = temp;
 
-    const topPriority = this.values.pop();
+		const topPriority = this.values.pop();
 
-    let parentIndex = 0;
-    let leftChildIndex = 2 * parentIndex + 1;
-    let rightChildIndex = 2 * parentIndex + 2;
-    let topPriorityChildIndex;
-    if (leftChildIndex > this.values.length - 1) {
-      if (rightChildIndex > this.values.length - 1) {
-        return topPriority;
-      }
-      topPriorityChildIndex = rightChildIndex;
-    } else if (rightChildIndex > this.values.length - 1) {
-      topPriorityChildIndex = leftChildIndex;
-    } else {
-      topPriorityChildIndex =
-        this.values[leftChildIndex].priority <
-        this.values[rightChildIndex].priority
-          ? leftChildIndex
-          : rightChildIndex;
-    }
+		let parentIndex = 0;
+		let leftChildIndex = 2 * parentIndex + 1;
+		let rightChildIndex = 2 * parentIndex + 2;
+		let topPriorityChildIndex;
+		if (leftChildIndex > this.values.length - 1) {
+			if (rightChildIndex > this.values.length - 1) {
+				return topPriority;
+			}
+			topPriorityChildIndex = rightChildIndex;
+		} else if (rightChildIndex > this.values.length - 1) {
+			topPriorityChildIndex = leftChildIndex;
+		} else {
+			topPriorityChildIndex =
+				this.values[leftChildIndex].priority <
+				this.values[rightChildIndex].priority
+					? leftChildIndex
+					: rightChildIndex;
+		}
 
-    // console.log(temp, parentIndex, topPriorityChildIndex, this.values);
+		// console.log(temp, parentIndex, topPriorityChildIndex, this.values);
 
-    while (
-      this.values[parentIndex].priority >
-      this.values[topPriorityChildIndex].priority
-    ) {
-      // console.log(temp, parentIndex, topPriorityChildIndex, this.values);
-      temp = this.values[parentIndex];
-      this.values[parentIndex] = this.values[topPriorityChildIndex];
-      this.values[topPriorityChildIndex] = temp;
-      parentIndex = topPriorityChildIndex;
-      leftChildIndex = 2 * parentIndex + 1;
-      rightChildIndex = 2 * parentIndex + 2;
-      if (leftChildIndex > this.values.length - 1) {
-        if (rightChildIndex > this.values.length - 1) {
-          break;
-        }
-        topPriorityChildIndex = rightChildIndex;
-      } else if (rightChildIndex > this.values.length - 1) {
-        topPriorityChildIndex = leftChildIndex;
-      } else {
-        topPriorityChildIndex =
-          this.values[leftChildIndex].priority <
-          this.values[rightChildIndex].priority
-            ? leftChildIndex
-            : rightChildIndex;
-      }
-    }
+		while (
+			this.values[parentIndex].priority >
+			this.values[topPriorityChildIndex].priority
+		) {
+			// console.log(temp, parentIndex, topPriorityChildIndex, this.values);
+			temp = this.values[parentIndex];
+			this.values[parentIndex] = this.values[
+				topPriorityChildIndex
+			];
+			this.values[topPriorityChildIndex] = temp;
+			parentIndex = topPriorityChildIndex;
+			leftChildIndex = 2 * parentIndex + 1;
+			rightChildIndex = 2 * parentIndex + 2;
+			if (leftChildIndex > this.values.length - 1) {
+				if (rightChildIndex > this.values.length - 1) {
+					break;
+				}
+				topPriorityChildIndex = rightChildIndex;
+			} else if (rightChildIndex > this.values.length - 1) {
+				topPriorityChildIndex = leftChildIndex;
+			} else {
+				topPriorityChildIndex =
+					this.values[leftChildIndex].priority <
+					this.values[rightChildIndex].priority
+						? leftChildIndex
+						: rightChildIndex;
+			}
+		}
 
-    return topPriority;
-  }
+		return topPriority;
+	}
 }
 
-const pq = new PriorityQueue();
+module.exports = PriorityQueue;
 
-pq.enqueue("mild fever", 5);
-pq.enqueue("concussion", 3);
-pq.enqueue("gun shot wound", 1);
-pq.enqueue("plasma rifle blast", 1);
-pq.enqueue("tummy ache :(", 5);
-pq.enqueue("bar fight", 2);
-pq.enqueue("ankle sprain", 4);
+// const pq = new PriorityQueue();
 
-console.log(pq);
+// pq.enqueue("mild fever", 5);
+// pq.enqueue("concussion", 3);
+// pq.enqueue("gun shot wound", 1);
+// pq.enqueue("plasma rifle blast", 1);
+// pq.enqueue("tummy ache :(", 5);
+// pq.enqueue("bar fight", 2);
+// pq.enqueue("ankle sprain", 4);
 
-console.log("---DEQUEUE----");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
-console.log(pq.dequeue());
-console.log(pq);
-console.log("***");
+// console.log(pq);
+
+// console.log("---DEQUEUE----");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
+// console.log(pq.dequeue());
+// console.log(pq);
+// console.log("***");
--- Section 26 - Graphs/dijkstra.js
+++ Section 26 - Graphs/dijkstra.js
@@ -1,3 +1,38 @@
 const { WeightedGraph } = require('./graph');
+const PriorityQueue = require('../Section 24 - Heaps/priorityQueue');
 
-function dijkstra(graph, v1, v2) {}
+function dijkstra(graph, start, end) {
+	const distances = {};
+	const queue = new PriorityQueue();
+	const previous = {};
+
+	for (let vertex in graph.adjacencyList) {
+		distances[vertex] = Infinity;
+		queue.enqueue(vertex, Infinity);
+		previous[vertex] = null;
+	}
+
+	distances[start] = 0;
+	queue.enqueue(start, 0);
+
+	console.log(distances, queue, previous);
+}
+
+const wg = new WeightedGraph();
+
+wg.addVertex('A');
+wg.addVertex('B');
+wg.addVertex('C');
+wg.addVertex('D');
+wg.addVertex('E');
+wg.addVertex('F');
+wg.addEdge('A', 'B', 4);
+wg.addEdge('A', 'C', 2);
+wg.addEdge('B', 'E', 3);
+wg.addEdge('C', 'D', 2);
+wg.addEdge('C', 'F', 4);
+wg.addEdge('D', 'E', 3);
+wg.addEdge('D', 'F', 1);
+wg.addEdge('E', 'F', 1);
+
+console.log(dijkstra(wg, 'A', 'E'));
