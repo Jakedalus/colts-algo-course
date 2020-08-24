642a80d1ec73150a190e7198d33290d43e97b0e5
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-24 16:09:35 -0400

    Add memoization version of fib

--- Section 29 - Dynamic Programming/fib.js
+++ Section 29 - Dynamic Programming/fib.js
@@ -1,15 +1,44 @@
 function fib(n) {
+	// console.log(n);
 	if (n <= 2) return 1;
 
 	return fib(n - 1) + fib(n - 2);
 }
 
-console.log('fib(3)', fib(3));
-console.log('fib(5)', fib(5));
-console.log('fib(7)', fib(7));
-console.log('fib(10)', fib(10));
-console.log('fib(15)', fib(15));
-console.log('fib(25)', fib(25));
-console.log('fib(26)', fib(26));
+// console.log('fib(3)', fib(3));
+// console.log('fib(5)', fib(5));
+// console.log('fib(7)', fib(7));
+// console.log('fib(10)', fib(10));
+// console.log('fib(15)', fib(15));
+// console.log('fib(25)', fib(25));
+// console.log('fib(26)', fib(26));
+// console.log('fib(30)', fib(30));
 console.log('fib(37)', fib(37));
-console.log('fib(40)', fib(40));
+// console.log('fib(40)', fib(40));
+// console.log('fib(42)', fib(42));
+
+function dynaProFib(n, memo = {}) {
+	// console.log('n:', n);
+	// console.log('memo:', memo);
+	// console.log('---');
+	// memo = memo || {};
+	if (memo[n]) return memo[n];
+	if (n <= 2) return 1;
+
+	return (memo[n] =
+		dynaProFib(n - 1, memo) + dynaProFib(n - 2, memo));
+}
+console.log('---- DYNAMIC ----');
+// console.log('dynaProFib(3)', dynaProFib(3));
+// console.log('dynaProFib(5)', dynaProFib(5));
+// console.log('dynaProFib(7)', dynaProFib(7));
+// console.log('dynaProFib(10)', dynaProFib(10));
+// console.log('dynaProFib(15)', dynaProFib(15));
+// console.log('dynaProFib(25)', dynaProFib(25));
+// console.log('dynaProFib(26)', dynaProFib(26));
+// console.log('dynaProFib(30)', dynaProFib(30));
+// console.log('dynaProFib(37)', dynaProFib(37));
+// console.log('dynaProFib(40)', dynaProFib(40));
+console.log('dynaProFib(42)', dynaProFib(42));
+console.log('dynaProFib(42)', dynaProFib(92));
+console.log('dynaProFib(42)', dynaProFib(920));
