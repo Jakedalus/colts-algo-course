9d0b047b349bd940864d16b8b11072855cf1e489
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-24 17:02:53 -0400

    Small correction to tabulation fib

--- Section 29 - Dynamic Programming/fib.js
+++ Section 29 - Dynamic Programming/fib.js
@@ -44,6 +44,7 @@ console.log('dynaProFib(42)', dynaProFib(92));
 console.log('dynaProFib(42)', dynaProFib(920));
 
 function tabFib(n) {
+	if (n <= 2) return 1;
 	const fibNums = [ 0, 1, 1 ];
 	for (let i = 3; i <= n; i++) {
 		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
